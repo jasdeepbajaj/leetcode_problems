2575b33b83ef529e3b2c1257b5f05e41fb2a156f
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-02-05 23:21:17 -0600

    Time: 16 ms (5.08%) | Memory: 13.4 MB (8.31%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        
+        
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<vector<int>> v1(n, vector<int>(m,0));
+
+        for(int i = 0; i<m; i++){
+            for(int j=0; j<n; j++)
+                v1[j][i] = matrix[i][j];
+        }
+
+        return v1;
+        
+    }
+};
