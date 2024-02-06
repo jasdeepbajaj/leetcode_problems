a28422da14db4d44f634bf5ae501698ac59efa74
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-02-05 23:23:59 -0600

    Time: 13 ms (21.56%) | Memory: 13.4 MB (8.31%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.cpp
+++ 898-transpose-matrix/transpose-matrix.cpp
@@ -2,18 +2,24 @@ class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
         
-        
-        
-        int m = matrix.size();
+        // Get the dimensions of the input matrix
+        int m = matrix.size(); 
         int n = matrix[0].size();
-
-        vector<vector<int>> v1(n, vector<int>(m,0));
-
+    
+        // Create an output matrix with transposed dimensions 
+        vector<vector<int>> v1(n, vector<int>(m,0)); 
+        
+        // Iterate through input matrix
         for(int i = 0; i<m; i++){
+            
+            // Iterate through columns
             for(int j=0; j<n; j++)
-                v1[j][i] = matrix[i][j];
+                
+                // Set value in transposed location
+                v1[j][i] = matrix[i][j]; 
         }
 
+        // Return transposed matrix
         return v1;
         
     }
