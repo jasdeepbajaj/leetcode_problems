1a70878a74dbbc22c5611944e0de85e1494c85ad
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-02-06 00:43:56 -0600

    Time: 0 ms (100.00%) | Memory: 12 MB (15.87%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -2,24 +2,26 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
 
+        // Get the dimensions of the input matrix
         int m = matrix.size();
         int n = matrix[0].size();
 
+        // Initialize pointers for matrix traversal. 
+        // Choosing bottom bottom left as initial point for matrix traversal. You can initialize using top right elements as well.
         int r = m-1, c = 0;
 
         bool found = false;
 
-        // Loop to search for the target value in the matrix
         while (r >= 0 && c < n) {
             if (matrix[r][c] == target) {
-                found = true;  // Set found flag to true if target is found
-                break; //get out of the while loop once the element has been found
+                found = true;  
+                break; 
             }
             else if (matrix[r][c] > target) {
-                r--;  // Move left in the current row if the current element is greater than the target
+                r--;  // Move up in the current column if the current element is greater than the target
             }
             else {
-                c++;  // Move down to the next row if the current element is less than the target
+                c++;  // Move right to the next column if the current element is less than the target
             }
         }
 
