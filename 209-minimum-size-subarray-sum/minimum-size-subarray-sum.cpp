faf833643a38450fe051b4bc236443d4022decce
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-05-16 16:03:13 -0500

    Time: 27 ms (57.70%) | Memory: 30.4 MB (99.39%) - LeetSync

---
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+
+        int l = 0;
+        int current_sum = 0;
+        int min_length = INT_MAX;
+
+        int check_sum  = accumulate(nums.begin(), nums.end(),0);
+
+        if(check_sum < target){
+            return 0;
+        }
+
+        for (int r = 0; r < nums.size(); r++){
+            current_sum += nums[r];
+
+            while (current_sum >= target){
+                min_length = min(min_length, r - l + 1);
+                current_sum -= nums[l];
+                ++l;
+            }
+        }
+
+        return min_length;
+        
+    }
+};
