e8e39d809a2fb9f9b2b2081d264279be6f6ec6d4
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-05-20 18:07:02 -0500

    Time: 38 ms (54.97%) | Memory: 16.6 MB (26.78%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,10 +1,9 @@
 class Solution:
     @lru_cache(maxsize=None)
     def fib(self, n: int) -> int:
-        ans = [0, 1]
+        if n == 0 or n == 1:
+            return n
 
-        for i in range(2, n+1):
-            ans.append(ans[i-2] + ans[i-1])
-
-        return ans[n]
+        else:
+            return self.fib(n-1) + self.fib(n-2)
         
