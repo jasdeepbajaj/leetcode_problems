5ec44602b63537633d3db5b18c026a6508e5a791
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-05-21 17:34:49 -0500

    Time: 35 ms (69.74%) | Memory: 16.6 MB (38.72%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,25 +1,26 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        odd_count = 0
+        sum_max_palindrome = 0
         ans = {}
-        # has_odd = False
+        has_odd = False
         for char in s:
             if char in ans:
                 ans[char] +=1
 
             else:
                 ans[char] = 1
-                
-            if ans[char] % 2  == 0:
-                odd_count -= 1
-                
-            else:
-                odd_count += 1
-        print(odd_count)
-        if odd_count > 1:
-            return len(s) - odd_count + 1
-            
-        else:
-            return len(s)
 
-            
+        for char, freq in ans.items():
+            if freq % 2 == 0:
+                sum_max_palindrome += freq
+
+            elif freq % 2 == 1:
+                sum_max_palindrome  += (freq-1)
+                has_odd = True
+                
+        if has_odd:
+            return sum_max_palindrome + 1
+        else:    
+            return sum_max_palindrome
+
+        
