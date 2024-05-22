ef735d627a0cae48fc76b688158f99ad87411ed7
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-05-22 00:07:24 -0500

    Time: 2239 ms (35.74%) | Memory: 55.4 MB (53.51%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -16,9 +16,9 @@ class Solution:
                     j += i
         
         # Count prime numbers by counting 0s in the is_not_prime list
-        prime_count = 0
-        for i in range(2, len(is_not_prime)):
-            if is_not_prime[i] == 0:
-                prime_count += 1
-
-        return prime_count
+        # prime_count = 0
+        # for i in range(2, len(is_not_prime)):
+        #     if is_not_prime[i] == 0:
+        #         prime_count += 1
+        return n - 2 - sum(is_not_prime)
+        # return prime_count
