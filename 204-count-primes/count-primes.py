8199644596ed5267415fc1fb8d3bf7aa6151415c
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-05-22 00:03:27 -0500

    Time: 2756 ms (25.75%) | Memory: 55.3 MB (53.51%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -1,19 +1,24 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
-        ans = [0] * n
-        for i in range(2,n):
-            if ans[i] == 0:
-                j = i*i
-                while j<n:
-                    ans[j] = 1
-                    j+=i
-                    # print(ans)
-        count = 0
-        for i in range(2, len(ans)):
-            if ans[i] == 0:
-                count += 1
-
-        return count
-
+        if n < 2:
+            return 0
         
+        # Initialize a list to mark non-prime numbers
+        is_not_prime = [0] * n
         
+        # Iterate from 2 to n-1 to mark non-prime numbers
+        for i in range(2, n):
+            if is_not_prime[i] == 0:  # If i is a prime number
+                # Mark multiples of i starting from i*i as non-prime
+                j = i * i
+                while j < n:
+                    is_not_prime[j] = 1
+                    j += i
+        
+        # Count prime numbers by counting 0s in the is_not_prime list
+        prime_count = 0
+        for i in range(2, len(is_not_prime)):
+            if is_not_prime[i] == 0:
+                prime_count += 1
+
+        return prime_count
