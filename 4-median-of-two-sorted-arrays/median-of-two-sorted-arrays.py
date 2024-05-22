6779b78e44d3d582e42e2e80d3805a86d0ffba95
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-05-21 23:38:12 -0500

    Time: 86 ms (27.96%) | Memory: 16.8 MB (82.31%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,40 +1,40 @@
+from typing import List
+
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        p1, p2 = 0, 0
-        m, n = len(nums1), len(nums2)
+        # Initialize pointers for nums1 and nums2
+        index1, index2 = 0, 0
+        # Get lengths of the two arrays
+        len1, len2 = len(nums1), len(nums2)
         
-        ans = [0] * (int((m+n)/2) + 1) 
-        # print(ans)
+        # Create a new array to store the first half of the merged arrays up to the median point
+        merged_half = [0] * (int((len1 + len2) / 2) + 1)
 
-        i = 0 
-        while i<len(ans):
-            if p1 < m and p2 < n:
-                if nums1[p1] < nums2[p2]:
-                    ans[i] = nums1[p1]
-                    i+=1
-                    p1+=1
-                    # print(ans)
-
+        i = 0
+        # Merge arrays until the merged_half array is filled
+        while i < len(merged_half):
+            if index1 < len1 and index2 < len2:
+                # Compare elements of both arrays and add the smaller one to merged_half
+                if nums1[index1] < nums2[index2]:
+                    merged_half[i] = nums1[index1]
+                    index1 += 1
                 else:
-                    ans[i] = nums2[p2]
-                    i+=1
-                    p2+=1
-                    # print(ans)
-
-            elif p1 == m:
-                ans[i] = nums2[p2]
-                i+=1
-                p2+=1
-
+                    merged_half[i] = nums2[index2]
+                    index2 += 1
+            # If all elements of nums1 are merged, add remaining elements of nums2
+            elif index1 == len1:
+                merged_half[i] = nums2[index2]
+                index2 += 1
+            # If all elements of nums2 are merged, add remaining elements of nums1
             else:
-                ans[i] = nums1[p1]
-                i+=1
-                p1+=1
-        # print(ans)
-        if (m+n)%2 == 0:
-            return (ans[-1] + ans[-2])/2
+                merged_half[i] = nums1[index1]
+                index1 += 1
+            i += 1
 
+        # Check if the combined length of nums1 and nums2 is even or odd
+        if (len1 + len2) % 2 == 0:
+            # If even, return the average of the two middle elements
+            return (merged_half[-1] + merged_half[-2]) / 2
         else:
-            return ans[-1]
-
-            
+            # If odd, return the middle element
+            return merged_half[-1]
