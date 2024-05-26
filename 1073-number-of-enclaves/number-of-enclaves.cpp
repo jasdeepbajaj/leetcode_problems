b4ad4a646cd9f48f6985f8692090a1d21992a5e8
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-05-26 18:15:21 -0500

    Time: 47 ms (77.85%) | Memory: 35.4 MB (43.90%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -0,0 +1,50 @@
+class Solution {
+private:
+    int m,n;
+    vector<pair<int, int>> movements = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<int>>& grid) {
+        vis[row][col] = 1;
+
+        for (auto& move : movements) {
+            int new_row = row + move.first;
+            int new_col = col + move.second;
+            if (new_row >= 0 && new_col >= 0 && new_row < m && new_col < n && grid[new_row][new_col] == 1 && !vis[new_row][new_col]) {
+                dfs(new_row, new_col, vis, grid);
+            }
+        }
+    }
+
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        m = grid.size();
+        // if (m == 0) return;
+        n = grid[0].size();
+        
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+
+        // Traverse the boundary cells
+        for (int i = 0; i < m; ++i) {
+            if (grid[i][0] == 1 && !vis[i][0]) dfs(i, 0, vis, grid);
+            if (grid[i][n-1] == 1 && !vis[i][n-1]) dfs(i, n-1, vis, grid);
+        }
+        for (int j = 0; j < n; ++j) {
+            if (grid[0][j] == 1 && !vis[0][j]) dfs(0, j, vis, grid);
+            if (grid[m-1][j] == 1 && !vis[m-1][j]) dfs(m-1, j, vis, grid);
+        }
+
+        int sum_grid = 0;
+        int sum_vis = 0;
+
+        // Iterate through the 2D vector and calculate the sum
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                sum_grid += grid[i][j];
+                sum_vis += vis[i][j];
+            }
+        }
+
+        return (sum_grid - sum_vis);
+        
+    }
+};
