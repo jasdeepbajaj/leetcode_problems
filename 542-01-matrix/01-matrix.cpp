7b1e0b849fe2900216b477c2e80ced4a0a663467
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-05-26 17:05:16 -0500

    Time: 59 ms (37.94%) | Memory: 38.1 MB (18.69%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -4,24 +4,27 @@ public:
         int m = mat.size();
         int n = mat[0].size();
         vector<vector<int>> vis(m, vector<int>(n,0));
-        vector<vector<int>> ans(m, vector<int>(n,INT_MAX));
+        vector<vector<int>> ans(m, vector<int>(n,0));
 
         vector<pair<int, int>> movements = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
-        queue<pair<int,int>> q;
+        queue<pair<pair<int,int>, int>> q;
         for(int i = 0; i<m; i++){
             for(int j=0; j<n; j++){
                 if (mat[i][j] == 0){
                     vis[i][j] = 1;
                     ans[i][j] = 0;
-                    q.push({i,j});
+                    q.push({{i,j},0});
                 }
             }
         }
 
         while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+
+            // ans[row][col] = steps;
             q.pop();
 
             for (auto itr: movements){
@@ -29,8 +32,8 @@ public:
                 int new_col = col + itr.second;
                 if (new_row>=0 && new_row<m && new_col>=0 && new_col<n && !vis[new_row][new_col]){
                     vis[new_row][new_col] = 1;
-                    ans[new_row][new_col] = min(ans[row][col] + 1, ans[new_row][new_col]);
-                    q.push({new_row, new_col});
+                    ans[new_row][new_col] = steps + 1;
+                    q.push({{new_row, new_col}, steps+1});
                 }
             }
 
