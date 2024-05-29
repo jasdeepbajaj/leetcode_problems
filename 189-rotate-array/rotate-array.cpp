7a7a5d480e74a4ce389948af4ba787ce01d4f065
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-05-29 15:40:46 -0500

    Time: 16 ms (86.44%) | Memory: 27.6 MB (35.94%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,13 +4,7 @@ public:
         
         int n = nums.size();
         k = k%n;
-        // if(new_k!=0){
-        //     for(int i = 0; i<new_k; i++){
-        //         int temp = nums[n-1];
-        //         nums.insert(nums.begin(), nums[n-1]);
-        //         nums.erase(nums.end()-1);
-        //     }
-        // }
+
         vector<int> temp;
         temp.insert(temp.end(), nums.end()-k, nums.end());
         for(int i = n-1; i>=k; i--){
