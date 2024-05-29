b39cf18da348f10a5cab4ca8db03995ad74712c6
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-05-29 12:05:06 -0500

    Time: 14 ms (15.09%) | Memory: 21.9 MB (6.02%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        vector<int> unique_nums;
-        // unique_nums.push_back(nums[0]);
-        int num_to_be_pushed = INT_MIN;
-        for(int i = 0; i<nums.size(); i++){
-            if(nums[i]>num_to_be_pushed) {
-                num_to_be_pushed = nums[i];
-                unique_nums.push_back(num_to_be_pushed);
-            }
-        }
+        map<int, int> hashh; //key = num, value = frequency
 
-        for(int i = 0; i<unique_nums.size(); i++){
-            cout<<unique_nums[i]<<" ";
+        for(int i = 0; i<nums.size(); i++){
+            hashh[nums[i]]++;
         }
-        
         nums.clear();
-        for(int i = 0; i<unique_nums.size(); i++){
-            nums.push_back(unique_nums[i]);
+        for(auto pair: hashh){
+            nums.push_back(pair.first);
         }
-        for(int i = 0; i<unique_nums.size(); i++){
-            cout<<unique_nums[i]<<" ";
-        }
-        return unique_nums.size();
+        return nums.size();
     }
 };
