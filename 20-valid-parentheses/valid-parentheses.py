d0129bda17f38db4fda35d6e459fbbcfcc792323
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-06-01 23:29:52 -0500

    Time: 37 ms (46.16%) | Memory: 16.5 MB (69.04%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,25 +1,27 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        map = {'(':')',
-        '{':'}',
-        '[':']',
-        }
+        # Dictionary to map opening brackets to their corresponding closing brackets
+        bracket_map = {'(': ')', '{': '}', '[': ']'}
 
-        if len(s)%2 != 0:
+        # If the length of the string is odd, it cannot be a valid sequence
+        if len(s) % 2 != 0:
             return False
- 
+
+        # Stack to keep track of opening brackets
         stack = []
 
+        # Iterate through each character in the string
         for bracket in s:
-            if bracket in map:
+            # If the character is an opening bracket, push it onto the stack
+            if bracket in bracket_map:
                 stack.append(bracket)
-
             else:
-                if len(stack)==0 or map[stack.pop()] != bracket :
+                # If the stack is empty or the top of the stack does not match the closing bracket, return False
+                if len(stack) == 0 or bracket_map[stack.pop()] != bracket:
                     return False
-        # print(stack)
+
+        # If the stack is not empty, there are unmatched opening brackets, return False
         if stack:
             return False
-        
         else:
             return True
