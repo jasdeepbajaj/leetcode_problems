cf4a9017dd4f9e4c770c5e2275843eca48ca74f0
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-06-02 16:08:05 -0500

    Time: 12 ms (68.08%) | Memory: 20.7 MB (26.45%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,20 +2,13 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        bool i_exist;
-        
-        for (int i = 0; i <= n; i++) {
-            i_exist = false;
-            for (int j = 0; j < n; j++) {
-                if (nums[j] == i) {
-                    i_exist = true;
-                    break; // No need to continue the inner loop if we found the number
-                }
-            }
-            if (!i_exist) return i;
+        vector<int> hash(n + 1, 0);
+        for (int i = 0; i < n; i++){
+            hash[nums[i]]++;
         }
-        
-        // This return statement is just a safety net and should never be reached
-        return -1;
+        for (int i = 0; i <= n; i++){
+            if (hash[i] == 0) return i;
+        }
+        return -1; 
     }
 };
