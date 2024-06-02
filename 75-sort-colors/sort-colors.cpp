794bf584b5201cb7e8be9977862ba862495d2ad2
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-06-02 13:24:00 -0500

    Time: 0 ms (100.00%) | Memory: 9.9 MB (33.55%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int count0 = 0;
-        int count1 = 0;
-        int count2 = 0;    
-        
-        for (int i = 0; i<nums.size(); i++){
-            if (nums[i] == 0) count0++;
-            else if (nums[i] ==1) count1++;
-            else  count2++;
+        //dutch national flag algorithm
+        // 0 to low-1 - 0 (extreme left)
+        //low to mid-1 
+        // mid to high - unsorted 0,1,2
+        //high + 1, n-1 - 2(extreme right)
+        int low = 0;
+        int mid = 0;
+        int high = nums.size()-1;
+
+        while (mid<=high){
+            if (nums[mid] == 0){
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
         }
-        for (int i = 0; i<count0; i++) nums[i] = 0;
-        for (int i = count0; i<count1 + count0; i++) nums[i] = 1;
-        for (int i = count1 + count0; i<count2+count1 + count0; i++) nums[i] = 2;
     }
 };
