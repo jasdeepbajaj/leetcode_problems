0a516508ce48a1e2da80fa9adadc029f004ca410
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-06-02 16:21:45 -0500

    Time: 11 ms (81.25%) | Memory: 20.4 MB (31.17%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -3,11 +3,9 @@ public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
         int p = 0;
-        for (int i = 1; i <= n; i++){
-            p^=i;
-        }
-        for (auto el:nums){
-            p^=el;
+        for (int i = 0; i < n; i++){
+            p^=(i+1);
+            p^=nums[i];
         }
 
         return p;
