7c99663f8baf4fa287c1bd9858193b8148300a77
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-06-03 17:54:30 -0500

    Time: 8 ms (14.73%) | Memory: 14.6 MB (14.57%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+
+        // find the breaking point
+        int index = -1;
+        for (int i = n-2; i>=0; i--){
+            if (nums[i]<nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+
+        // If break point does not exist:
+        if (index == -1){
+            //reverese the whole array
+            reverse(nums.begin(), nums.end());
+        }
+
+        // Step 2: Find the next greater element and swap it with arr[ind]:
+        else{
+            for (int i = n-1; i>index;i--){
+                if (nums[i] > nums[index]){
+                    swap(nums[i], nums[index]);
+                    break;
+                }
+
+            }
+        //reverse after the breaking index
+            reverse(nums.begin()+index+1, nums.end());
+
+
+        }
+        
+
+
+
+
+    }
+};
