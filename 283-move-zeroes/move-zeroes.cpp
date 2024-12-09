e02c22c0b98fef07c3081451fbc2195ac006eddd
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-12-09 17:46:12 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (16.50%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = -1;
+
+        int j = -1; //pointer to remain on zeros
         for (int i = 0; i<nums.size(); i++){
-            if (nums[i]==0){
+            if (nums[i] == 0){
                 j = i;
                 break;
-            }
+            } 
         }
 
-        if (j!=-1){
-            for (int i = j+1; i<nums.size(); i++){
+        if (j != -1){
+            for (int i = j + 1; i < nums.size(); ++i){
                 if (nums[i] != 0){
-                    int temp = nums[j];
-                    nums[j] = nums[i];
-                    nums[i] = temp;
+                    swap(nums[i], nums[j]);
                     j++;
                 }
             }
         }
+
+        
     }
 };
