93c0b44c3bbee39eadce66e43e1ddc8e8232990a
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-12-09 14:45:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (11.04%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -2,35 +2,27 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         int n = nums.size();
-        if (n <= 1) return true;  // Edge case: a single element is trivially sorted
+        if (n <= 1) return true;
 
         int pivot_ind = -1;
-        bool found_decrease = false;
+        int count = 0;
 
-        // Find the pivot (where the array starts decreasing)
         for (int i = 1; i < n; ++i) {
             if (nums[i] < nums[i - 1]) {
-                if (found_decrease) {
-                    // If we find a second decrease, return false
-                    return false;
-                }
-                found_decrease = true;
+                count++;
                 pivot_ind = i;
             }
         }
 
-        // If no decrease was found, the array is already sorted
-        if (!found_decrease) return true;
+        if (count==0) return true;
+        else if(count>1) return false;
 
-        // After finding the pivot, check if the rest of the array is non-decreasing
         for (int i = pivot_ind + 1; i < n; ++i) {
             if (nums[i] < nums[i - 1]) {
                 return false;
             }
         }
 
-        // Finally, check if the last element is less than or equal to the first element
-        // to ensure the array can be rotated to become non-decreasing
         if (nums[n - 1] > nums[0]) {
             return false;
         }
