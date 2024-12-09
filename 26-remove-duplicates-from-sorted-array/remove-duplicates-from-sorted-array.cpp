fa8d66b1e494a5f45654f877fa15c582756a26b2
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-12-09 17:13:25 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (11.85%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // Initialize a pointer i to track the position of unique elements
         int i = 0;
-        
-        // Iterate through the array starting from the second element
-        for (int j = 1; j < nums.size(); j++) {
-            // If the current element is different from the last unique element
-            if (nums[j] != nums[i]) {
-                // Move the pointer i forward and update the next unique position with the current element
-                nums[i + 1] = nums[j];
-                i++;  // Increment the unique element index
+        for (int j = 1; j<nums.size(); ++j){
+            if (nums[j] != nums[i]){
+                nums[i+1] = nums[j];
+                i ++;
             }
         }
-        
-        // Return the number of unique elements (i + 1 since i is zero-indexed)
-        return i + 1;
+        return i+1;
     }
 };
