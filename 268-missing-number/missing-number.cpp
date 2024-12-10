8eccbb928ceaa6a43896fe8695d7780a2e99d34c
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-12-10 07:13:05 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (18.88%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -4,14 +4,20 @@ public:
       // Using XOR
       int n = nums.size();
       int xor1 = 0;
-      for(int i = 0; i<=n;++i){
-        xor1 = xor1^i;
-      } 
-
       int xor2 = 0;
-      for(int num : nums){
-        xor2 = xor2^num;
-      }
+    //   for(int i = 0; i<=n;++i){ //[0,3]
+    //     xor1 = xor1^i;
+    //   } 
+
+    //   for(int num : nums){
+    //     xor2 = xor2^num;
+    //   }
+
+      for(int i = 0; i<n;++i){ //[0,3)
+        xor1 = xor1^i;
+        xor2 = xor2^nums[i];
+      } 
+      xor1 = xor1^n;
 
       return xor1^xor2;
     }
