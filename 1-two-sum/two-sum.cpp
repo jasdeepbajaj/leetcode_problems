6845501f2f9feda42a3aac3d7b2acac4ba56bf57
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2024-12-10 09:00:13 +0530

    Time: 38 ms (42.84%) | Memory: 14.1 MB (55.12%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // Create an unordered map to store the elements of the array and their indices
-        unordered_map<int, int> dict; // element, index
-        
-        // Iterate through the array
-        for (int i = 0; i < nums.size(); i++) {
-            // Calculate the difference needed to reach the target
-            int diff_required = target - nums[i];
-            
-            // Check if the required difference is already in the map
-            if (dict.find(diff_required) != dict.end()) {
-                // If it is, return the current index and the index of the difference
-                return {i, dict[diff_required]};
-            } else {
-                // If not, add the current element and its index to the map
-                dict[nums[i]] = i;
+        int n = nums.size();
+        for(auto i = 0; i < n; i++){
+            for (int j = i+1; j<n; j++){
+                if (nums[i] + nums[j] == target){
+                   return {i,j}; 
+                }
             }
         }
-        
-        // Return an empty vector if no solution is found
-        return {};
+
+        return {-1,-1};
     }
 };
