567c38b77e5b877d98c53575f3565ccc9df59fd2
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2025-02-20 00:58:31 -0600

    Time: 30 ms (97.96%) | Memory: 46.8 MB (41.42%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,16 +1,35 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int count = 0;
-        for(int i = 0; i<nums.size(); i++){
-            int sum = 0;
-            for (int j = i; j< nums.size(); j++){
-                sum += nums[j];
-                if (sum == k){
-                    count++;
-                }
-            }
+        int n = nums.size();
+        vector<int> prefix_sum(n, 0);
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            prefix_sum[i] = sum;
         }
+
+        unordered_map<int, int> dict; // prefix_sum, freq
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (prefix_sum[i] == k) count++;
+            
+            // Corrected the diff calculation
+            int diff = prefix_sum[i] - k;
+
+            if (dict.find(diff) != dict.end()) {
+                count += dict[diff];
+            }
+            
+            dict[prefix_sum[i]]++;
+        }
+
         return count;
     }
 };
