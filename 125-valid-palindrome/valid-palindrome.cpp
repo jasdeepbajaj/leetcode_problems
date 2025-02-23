ee0af026a9017e10baf4ca4fc22600d37e209687
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2025-02-22 18:24:52 -0600

    Time: 0 ms (100.00%) | Memory: 9.7 MB (93.88%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,41 +1,19 @@
 class Solution {
-
-private:
-    bool check_palindrome(string s, int n) {
-        for (int i = 0; i < n / 2; i++) {
-            if (s[i] != s[n - i - 1]) {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.size();
+        while (left < right){
+            while (left < right && !isalnum(s[left])) left++;
+            while (left < right && !isalnum(s[right])) right--;
+            if (tolower(s[left]) == tolower(s[right])){
+                left++;
+                right--;
+            }
+            else{
                 return false;
             }
         }
-        return true;
-    }
-    
-    bool isSpecialCharacter(char ch) {
-        if (ch >= 'A' && ch <= 'Z') return false;
-        if (ch >= 'a' && ch <= 'z') return false;
-        if (ch >= '0' && ch <= '9') return false;
-        return true;
-    }
-    
-    char toLowercase(char ch) {
-        if (ch >= 'A' && ch <= 'Z') {
-            return ch + ('a' - 'A');
-        }
-        return ch;
-    }
 
-public:
-    bool isPalindrome(string s) {
-        int n = s.size();
-        string s_;
-        
-        for (int i = 0; i < n; i++) {
-            if (!isSpecialCharacter(s[i])) {
-                s_ += toLowercase(s[i]);
-            }
-        }
-        
-        int new_n = s_.size();
-        return check_palindrome(s_, new_n);
+        return true;
     }
 };
