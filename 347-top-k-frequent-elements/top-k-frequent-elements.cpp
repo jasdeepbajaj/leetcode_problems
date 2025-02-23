b1f3dae89ddefa15105c127fd2c18d528b94b704
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2025-02-23 02:09:43 -0600

    Time: 5 ms (27.63%) | Memory: 19.9 MB (10.99%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // Map to count frequency of each number
+        unordered_map<int, int> frequencyMap;
+        
+        // Bucket array where index represents frequency
+        vector<vector<int>> buckets(nums.size() + 1);
+
+        // Count the frequency for each number in nums
+        for (int num : nums) {
+            frequencyMap[num]++;
+        }
+        
+        // Place numbers into corresponding bucket based on frequency count
+        for (const auto& pair : frequencyMap) {
+            int number = pair.first;
+            int freq = pair.second;
+            buckets[freq].push_back(number);
+        }
+        
+        vector<int> result;
+        // Iterate buckets in reverse (highest frequency first)
+        for (int freq = buckets.size() - 1; freq > 0 && result.size() < k; --freq) {
+            // Add each number from the current bucket into result
+            for (int number : buckets[freq]) {
+                result.push_back(number);
+                if (result.size() == k)
+                    break;  // Stop if we have collected k numbers
+            }
+        }
+        return result;
+    }
+};
