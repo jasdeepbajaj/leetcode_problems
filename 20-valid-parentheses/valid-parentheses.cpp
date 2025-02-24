0cc11fab7c22287dcba86a1ba80e267322f96f48
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2025-02-24 01:02:30 -0600

    Time: 0 ms (100.00%) | Memory: 9 MB (21.31%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
     bool isValid(string s) {
-        // Unordered map to store the matching pairs of brackets
         unordered_map<char, char> bracket_map = {{'(', ')'}, {'{', '}'}, {'[', ']'}};
 
-        // If the length of the string is odd, it cannot be a valid sequence
-        if (s.length() % 2 != 0) {
-            return false;
-        }
+        if (s.length() % 2 != 0) return false;
 
-        // Stack to keep track of opening brackets
-        stack<char> stack;
+        stack<char> st;
 
-        // Iterate through each character in the string
         for (char bracket : s) {
-            // If the character is an opening bracket, push it onto the stack
-            if (bracket_map.find(bracket) != bracket_map.end()) {
-                stack.push(bracket);
-            } else {
-                // If the stack is empty or the top of the stack does not match the closing bracket, return False
-                if (stack.empty() || bracket_map[stack.top()] != bracket) {
-                    return false;
-                }
-                stack.pop();
+            if (bracket_map.find(bracket) != bracket_map.end()) st.push(bracket);
+            
+            else {
+                if (st.empty() || bracket_map[st.top()] != bracket) return false;
+                st.pop();
             }
         }
-
-        // If the stack is not empty, there are unmatched opening brackets, return False
-        if (!stack.empty()) {
-            return false;
-        } else {
-            return true;
-        }
+        return st.empty();
     }
 };
