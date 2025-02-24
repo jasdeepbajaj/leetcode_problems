a0da1aa13539b01d14c740a6413ae5da63a6c82d
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2025-02-23 22:06:03 -0600

    Time: 4 ms (18.57%) | Memory: 97.5 MB (25.53%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // Print the size of the prices vector for debugging purposes
-        cout << prices.size();
-        
-        // Initialize the minimum buying price to the first price in the array
-        int min_buy = prices[0];
-        
-        // Initialize the maximum profit to 0
-        int max_prof = 0;
-        
-        // Iterate over the prices starting from the second price
-        for(int i = 1; i < prices.size(); i++) {
-            // Calculate the potential profit if the stock was sold on day i
-            int diff = prices[i] - min_buy;
-            
-            // Update the maximum profit if the current profit is greater
-            max_prof = max(max_prof, diff);
-            
-            // Update the minimum buying price to the lowest price so far
-            min_buy = min(min_buy, prices[i]);
-        }
-        
-        // Return the maximum profit found
-        return max_prof;
+        int n = prices.size();
+        int left_buy = 0;
+        int right_sell = 1;
+        int maxProfit = 0;
+        while (right_sell < n){
+            if (prices[right_sell] > prices[left_buy]){
+                int profit =  prices[right_sell] - prices[left_buy];
+                maxProfit = max(profit, maxProfit);
+            }
+            else{
+                left_buy = right_sell;
+            }
+            right_sell++;
+        }   
+        return maxProfit;
     }
 };
