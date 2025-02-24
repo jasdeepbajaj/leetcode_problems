102c4fa5cd174c313d197fa53e052cc90fae0f28
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2025-02-24 02:31:41 -0600

    Time: 33 ms (24.36%) | Memory: 111.8 MB (16.21%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -3,15 +3,16 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n = temperatures.size();
         vector<int> ans(n, 0);
-        stack<int> st; // Store indices
+        stack<pair<int, int>> st; // Stores (temperature, index)
         
         for (int i = 0; i < n; i++) {
-            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
-                int idx = st.top();
+            int curr_temp = temperatures[i];
+            while (!st.empty() && st.top().first < curr_temp) {
+                auto topPair = st.top();
                 st.pop();
-                ans[idx] = i - idx;
+                ans[topPair.second] = i - topPair.second;
             }
-            st.push(i);
+            st.push({curr_temp, i});
         }
         return ans;
     }
