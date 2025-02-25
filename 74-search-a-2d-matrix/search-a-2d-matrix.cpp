b03651f27c77613283b172d4707e8c2595d4da4e
Jasdeep Bajaj <jasdeepbajaj3@tamu.edu>
2025-02-24 21:00:33 -0600

    Time: 0 ms (100.00%) | Memory: 13.3 MB (76.09%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-
-        // Get the dimensions of the input matrix
         int m = matrix.size();
         int n = matrix[0].size();
 
-        // Initialize pointers for matrix traversal. 
-        // Choosing bottom bottom left as initial point for matrix traversal. You can initialize using top right elements as well.
-        int r = m-1, c = 0;
+        int curr_row = m - 1;
+        int curr_col = 0;
 
-        bool found = false;
-
-        while (r >= 0 && c < n) {
-            if (matrix[r][c] == target) {
-                found = true;  
-                break; 
-            }
-            else if (matrix[r][c] > target) {
-                r--;  // Move up in the current column if the current element is greater than the target
-            }
-            else {
-                c++;  // Move right to the next column if the current element is less than the target
-            }
+        while(curr_row >= 0 && curr_col < n){
+            if (matrix[curr_row][curr_col] == target) return true;
+            else if (matrix[curr_row][curr_col] > target) curr_row--;
+            else curr_col++;
         }
 
-        return found;
-        
+        return false;
     }
 };
